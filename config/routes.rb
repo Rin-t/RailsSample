a2b53865a65aad1cafd1791aef84228aa4033d3b
Rin-t <rin.t.4921@gmail.com>
2025-09-16 22:38:07 +0900

    graphqlのgenerateした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
   post "users/create"  # POSTに変更（フォーム送信用）
   get "users", to: "users#index"
   delete "users/:id", to: "users#destroy", as: :user
